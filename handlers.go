d88e20cf65fd443622934d403c13d41a9877c20b
hypersleep <moiplov@gmail.com>
2015-05-15 19:09:12 +0300

    ffjson added

--- handlers.go
+++ handlers.go
@@ -3,20 +3,39 @@ package main
 import(
 	"fmt"
 	"net/http"
+
+	"github.com/pquerna/ffjson/ffjson"
 )
 
+type Data struct {
+	Message []string `json:"message"`
+}
+
+type Status struct {
+	Status  bool `json:"status"`
+	Data    Data `json:"data"`
+}
+
 func RegistrationHandler(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "Hello")
+	rw.Header().Set("Content-Type", "application/json")
+	buf, _ := ffjson.Marshal(&Status{ true, Data{} })
+	fmt.Fprintln(rw, string(buf))
 }
 
 func SignInHandler(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "Hello")
+	rw.Header().Set("Content-Type", "application/json")
+	buf, _ := ffjson.Marshal(&Status{ true, Data{} })
+	fmt.Fprintln(rw, string(buf))
 }
 
 func LogOutHandler(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "Hello")
+	rw.Header().Set("Content-Type", "application/json")
+	buf, _ := ffjson.Marshal(&Status{ true, Data{} })
+	fmt.Fprintln(rw, string(buf))
 }
 
 func CheckHandler(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(rw, "Hello")
+	rw.Header().Set("Content-Type", "application/json")
+	buf, _ := ffjson.Marshal(&Status{ true, Data{} })
+	fmt.Fprintln(rw, string(buf))
 }
