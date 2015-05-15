1740cd8445196ded232a6a27c745d67ebe323c98
hypersleep <moiplov@gmail.com>
2015-05-15 17:00:13 +0300

    Cassandra added, cassandra connection timeout, app running in docker containers

--- main.go
+++ main.go
@@ -2,12 +2,44 @@ package main
 
 import(
 	"fmt"
+	"log"
+	"time"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/gocql/gocql"
 )
 
+var cassandraSession *gocql.Session
+
 func main() {
+	var cassandraSessionChannel chan *gocql.Session = make(chan *gocql.Session)
+
+	go func() {
+		CassandraLoop:
+			for {
+				cluster := gocql.NewCluster("cassandra")
+				session, err := cluster.CreateSession()
+
+				if err != nil {
+					log.Println("Can't connect cassandra! Try Again", err)
+					time.Sleep(500 * time.Millisecond)
+				} else {
+					cassandraSessionChannel <- session
+					break CassandraLoop
+				}
+			}
+	}()
+
+	select {
+		case cassandraSession = <- cassandraSessionChannel:
+			log.Println("Cassandra connection established!")
+		case <- time.After(10 * time.Second):
+			log.Fatal("Can't connect to Cassadra! Quit by timeout!")
+	}
+
+	defer cassandraSession.Close()
+
 	port := "8080"
 
 	r := mux.NewRouter()
